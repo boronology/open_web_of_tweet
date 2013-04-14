06a527a9bde2d1002845f48d74d5f95dfec7be3c
boronji <nakaura01@hotmail.com>
2013-04-14 22:38:16 +0900

    ぺんぎんさんのをお借りしてとりあえず作った

---
+++ open_web_of_tweet.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# 公式Webで開く
+
+Plugin.create :open_favstar_of_tweet do
+
+  command(:open_web_of_tweet,
+          name: 'このツイートをWebで開く',
+          condition: Plugin::Command[:HasMessage],
+          visible: true,
+          role: :timeline) do |opt|
+    opt.messages.each do |m|
+      Gtk::openurl("https://twitter.com/#{m.user.idname}/status/#{m.id}")
+    end
+  end
+
+end
